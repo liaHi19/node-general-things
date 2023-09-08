2d7f8aaedf2a9e35cd31b2c7987e736514d5794b
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-09-08 08:45:38 +0100

    Add example with server

--- events.js
+++ events.js
@@ -1,4 +1,5 @@
 const EventEmitter = require("events");
+const http = require("http");
 
 const myEmitter = new EventEmitter();
 
@@ -14,3 +15,19 @@ myEmitter.on("newSale", (stock) => {
   console.log(`There are ${stock} items in the stock`);
 });
 myEmitter.emit("newSale", 9);
+
+////////////
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  res.end("Request received");
+});
+
+server.on("request", (req, res) => {
+  console.log("Another request");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("server is listening");
+});
