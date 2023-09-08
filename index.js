75979fa5cc7cc956d45ed408bc6d109770a87ed1
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-09-08 15:18:24 +0100

    Add workers

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+const worker = new Worker("worker.js");
+
+const sumButton = document.querySelector("#sumButton");
+const bgButton = document.querySelector("#bgButton");
+
+sumButton.addEventListener("click", () => {
+  worker.postMessage("count sum");
+});
+
+worker.onmessage = (message) => {
+  alert(`sum is ${message.data}`);
+};
+
+bgButton.addEventListener("click", () => {
+  if (document.body.style.background !== "green") {
+    document.body.style.background = "green";
+  } else {
+    document.body.style.background = "blue";
+  }
+});
