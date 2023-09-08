294529953e875d823821baa8f9ca4dd0c1693a04
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-09-08 16:55:25 +0100

    Add example with resizing images

---
+++ resize-worker.js
@@ -0,0 +1,15 @@
+const { parentPort, workerData } = require("worker_threads");
+const sharp = require("sharp");
+
+const { src, width, height } = workerData;
+const [filename, ext] = src.split(".");
+
+console.log(`Resizing ${src} to ${width}px wide`);
+
+const resize = async () => {
+  await sharp(src)
+    .resize(width, height, { fit: "cover" })
+    .toFile(`${src}-${width}.${ext}`);
+};
+
+resize();
